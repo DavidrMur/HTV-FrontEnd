4d7487f45a39421b0354d1a368032453437cde8e
Ryan Tong <56896048+ryyvntong@users.noreply.github.com>
2020-02-15 23:38:33 -0500

    front end mapping

--- screens/linkDisplayScreen.js
+++ screens/linkDisplayScreen.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-
+import WebView from 'react-native-webview'
 const LinkDisplayScreen = props =>{
     return(
-        <View>
-            <Text>Display Screen!!</Text>
-        </View>
-    )
+    <View>
+        <WebView
+        source={{ uri: 'https://www.youtube.com/embed/ZZ5LpwO-An4' }}
+        style={{ marginTop: 20 }}
+        />
+    </View>
+        
+    );
 }
+let styles = StyleSheet.create({
+    cardRounded: {
+        paddingEnd:0,  paddingTop: 0 , paddingBottom: 0, paddingStart: 0, padding:0,
+        borderRadius: 12, alignItems:"center",flex:0, height:240,
+    },
+
+})
 
 export default LinkDisplayScreen
